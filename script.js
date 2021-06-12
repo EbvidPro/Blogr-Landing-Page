bfec9ec8b7191b6ca19941163c08cd686b65c09f
EbvidPro <davidebenezer93@gmail.com>
2021-06-12 18:07:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 function prodDropdown() {
-    var x = document.getElementById('prod');
-    if (x.style.display === 'block') {
-        return x.style.display = 'none';
-    } else {
-        return x.classList.toggle('show');
-    }
+    var x = document.getElementById('prod').classList.toggle('show');
 }
 
 function comDropdown() {
-    var x = document.getElementById('com');
-    if (x.style.display === 'block') {
-        return x.style.display = 'none';
-    } else {
-        return x.classList.toggle('show');
-    }
+    var x = document.getElementById('com').classList.toggle('show');
 }
 
 function conDropdown() {
-    var x = document.getElementById('con');
-    if (x.style.display === 'block') {
-        return x.style.display = 'none';
-    } else {
-        return x.classList.toggle('show');
+    var x = document.getElementById('con').classList.toggle('show');
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.product')) {
+        var dropdowns = document.getElementById('prod');
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
     }
 }
 
