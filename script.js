2015ce6affa151585faeeb45c0e6f0eaa0007ce4
EbvidPro <davidebenezer93@gmail.com>
2021-06-27 17:23:21 +0100

    Update

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 function prodDropdown() {
     var x = document.getElementById('prod').classList.toggle('show');
-}
+};
 
 function comDropdown() {
     var x = document.getElementById('com').classList.toggle('show');
-}
+};
 
 function conDropdown() {
     var x = document.getElementById('con').classList.toggle('show');
-}
+};
 
 
-let hamburger = document.querySelector('.hamburg');
 function menu() {
-    let attribute = hamburger.getAttribute('src');
-    let navigationPanel = document.querySelector('.nav');
-    if (attribute == "icon-hamburger.svg"){
-        hamburger.setAttribute("src","icon-close.svg")
-        navigationPanel.style.display = "grid"
-    } else {
-        hamburger.setAttribute("src", "icon-hamburger.svg")
-        navigationPanel.style.display = "none";
-    }
-}
+    let hamburger = document.querySelector(".hamburg");
+    let attribute = hamburger.getAttribute("src");
+    let navigationPanel = document.querySelector(".nav");
+
+        if(attribute == "icon-hamburger.svg"){
+            hamburger.setAttribute("src","icon-close.svg")
+            navigationPanel.style.display = "grid"
+        }else{
+            hamburger.setAttribute("src", "icon-hamburger.svg")
+            navigationPanel.style.display = "none";
+        }
+};
